5262b50511e04b9bd0c008024cf2be5e3493bce7
smilingpigs <ajaiprakaz@gmail.com>
2023-11-16 01:36:48 +0530

    day 1

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(
-    appBar: AppBar(title: const Text('SOE'),
-    backgroundColor: Colors.lightGreen,),
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('SOE'),
+      backgroundColor: Colors.lightGreen,
+    ),
     body: Container(
       decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Colors.lightGreen, Colors.teal]
-
-        )
-      ),
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.lightGreen, Colors.teal])),
       child: const Center(
-        child: Text('Hospitaly',
-        style: TextStyle(color: Colors.green,
-        fontSize: 90),),
+        child: Text(
+          'Hospitaly',
+          style: TextStyle(color: Colors.green, fontSize: 90),
+        ),
       ),
     ),
   )));
